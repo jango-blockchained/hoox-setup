4f327b29990761acd776fb0f99239d66c594651a
jango-blockchained <info@cryptolinx.de>
2026-04-26 22:00:10 +0200

    fix(shared): use explicit named exports in index

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,4 @@
-export * from "./types.js";
-export * from "./json-response.js";
-export * from "./exchange-client.js";
+export type { StandardResponse, ApiSuccessResponse, ApiErrorResponse, ApiResponse } from "./types.js";
+export { createJsonResponse, createSuccessResponse, createErrorResponse } from "./json-response.js";
+export { BaseExchangeClient } from "./exchange-client.js";
+export type { ExchangeName, ExchangeConfig, TradeParams, OrderResponse, Position } from "./exchange-client.js";
