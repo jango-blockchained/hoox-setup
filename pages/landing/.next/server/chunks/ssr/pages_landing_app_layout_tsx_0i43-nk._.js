a5f081cb97ff8ef0f8c3c7599e949d7beb2ecc48
Jango <info@inart-designs.de>
2026-04-27 23:09:33 +0200

    feat(landing): initialize Next.js app

---
+++ pages/landing/.next/server/chunks/ssr/pages_landing_app_layout_tsx_0i43-nk._.js
@@ -0,0 +1,3 @@
+module.exports=[28689,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"en",children:(0,b.jsx)("body",{children:a})})}])},98032,a=>{a.n(a.i(28689))}];
+
+//# sourceMappingURL=pages_landing_app_layout_tsx_0i43-nk._.js.map
---
+++ pages/landing/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hoox Landing Page</h1>
+    </main>
+  );
+}
