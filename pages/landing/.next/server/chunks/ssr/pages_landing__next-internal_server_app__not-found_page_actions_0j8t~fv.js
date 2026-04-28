31967399247a4032bacccef503b308d433b17ca0
Jango <info@inart-designs.de>
2026-04-28 13:56:23 +0200

    remove: delete pages/landing and packages/ui directories

--- pages/landing/.next/server/chunks/ssr/pages_landing__next-internal_server_app__not-found_page_actions_0j8t~fv.js
+++
@@ -1,3 +0,0 @@
-module.exports=[21881,(a,b,c)=>{}];
-
-//# sourceMappingURL=pages_landing__next-internal_server_app__not-found_page_actions_0j8t~fv.js.map
--- pages/landing/.next/server/chunks/ssr/pages_landing__next-internal_server_app_page_actions_03~uhzw.js
+++
@@ -1,3 +0,0 @@
-module.exports=[17378,(a,b,c)=>{}];
-
-//# sourceMappingURL=pages_landing__next-internal_server_app_page_actions_03~uhzw.js.map
